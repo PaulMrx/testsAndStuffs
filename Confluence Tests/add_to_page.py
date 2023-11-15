a634386f3aa56e101f4a06d3c1aa4f89f98fdde0
PaulMrx <120225682+PaulMrx@users.noreply.github.com>
2023-11-15 02:15:12 +0000

    commit

--- Confluence Tests/add_to_page.py
+++ Confluence Tests/add_to_page.py
@@ -1,13 +1,16 @@
 import json
 from atlassian import Confluence
 
-passWord = json.load("C:\Users\opusp\OneDrive\Desktop\confluence_token.json.txt")
+f = open(r"C:\Users\opusp\OneDrive\Desktop\confluence_token.json")
+passWord = json.load(f)
 
-confluence = Confluence(
-    url = "https://opus-fx.atlassian.net",
-    username = "opus.paulmoriaux@gmail.com",
-    password = passWord
-)
+print(f)
+
+# confluence = Confluence(
+#     url = "https://opus-fx.atlassian.net",
+#     username = "opus.paulmoriaux@gmail.com",
+#     password = passWord
+# )
 
 
-confluence.append_page("98412", "Test for HDA Discoverability", "I'm adding this text")
+# confluence.append_page("98412", "Test for HDA Discoverability", "I'm adding this text")
