2679d0040ac8d082661a989dd5c467c9289f5d46
PaulMrx <120225682+PaulMrx@users.noreply.github.com>
2023-11-15 02:07:21 +0000

    commit all

--- Confluence Tests/add_to_page.py
+++ Confluence Tests/add_to_page.py
@@ -1,9 +1,12 @@
+import json
 from atlassian import Confluence
+
+passWord = json.load("C:\Users\opusp\OneDrive\Desktop\confluence_token.json.txt")
 
 confluence = Confluence(
     url = "https://opus-fx.atlassian.net",
     username = "opus.paulmoriaux@gmail.com",
-    password = "ATATT3xFfGF0SonOK9WIsUk7VlicVF1iDwzRgRPmNyI5mVyKEfmmZ6IpwYQ_wbVuxUW58BRv2l23Z6HwTjbcYsfLtbziNUiHOWZGNFIk2gaY9Ecjbfucecxi-aZ-as4mzj8SHg8S-m5C0uETlQJNIC_8iiZt_OrwGSrxqw0NgBKJcvVGzyYTwvc=D0AFEB8E"
+    password = passWord
 )
 
 
